d7b386eb86f66840fa21c5ecb5ffb7ded2a18f49
v_jpch <v_jpch.digitalgd.com>
2024-11-13 10:31:28 +0800

    chore: 更新部署脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "你好， 欢迎使用 cjp-cli-dev 脚手架 ！",
   description: "这是我的第一个 VuePress 站点",
-  base: "/cjp-cli-dev/", // 部署后的路径前缀
+  base: "/cjp-cli-dev-docs/", // 部署后的路径前缀
   home: "/", // 首页的路径，用于导航栏中 Logo 的链接、404 页面的 返回首页 链接
   head: [],
   theme: defaultTheme({
